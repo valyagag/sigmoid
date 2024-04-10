cba728d0e2dfcf1a47c206c54c47057cdc58f40d
valyagag <valentina.gagauz17@gmail.com>
2024-04-10 13:41:52 +0300

    lesson1

---
+++ task_1.py
@@ -0,0 +1,25 @@
+# Aceasta este prima ta sarcină legată de variabile în python
+# Creează o variabilă numită `age` și seteaz-o la vârsta ta
+
+age=29
+
+
+# Acum afișează valoarea variabilei `age` folosind funcția `print`
+print(age)
+
+
+# Acum schimbă valoarea variabilei `age` la vârsta prietenului tău și afișează valoarea variabilei `age` folosind funcția `print`
+age=30
+print(age)
+
+
+# Acum creează o nouă variabilă numită `age2` și seteaz-o la valoarea variabilei `age` și afișează valoarea variabilei `age2` folosind funcția `print`
+
+age2=32
+print(age2)
+
+# Acum șterge variabila `age` și încearcă să afișezi valoarea variabilei `age` folosind funcția `print`
+print(age)
+
+# Asta a fost tot pentru această sarcină
+ 
