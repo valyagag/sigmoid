f98b9615fc9a8919e26c4428bd2c108774f8d39b
valyagag <valentina.gagauz17@gmail.com>
2024-04-24 12:30:28 +0300

    7

--- lessons/lesson7/task_1 (4).py
+++ lessons/lesson7/task_1 (4).py
@@ -1,53 +1,72 @@
-# Aceasta este sarcina pentru lecția 7 legată de comentarii, continuarea liniilor și instrucțiuni condiționale.
-
 # Creați o variabilă numită number și atribuiți-i o valoare întreagă.
 
 # CODUL TĂU VINE MAI JOS:
-
+number=2
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este pozitiv și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number>0 :
+    print ('numărul este pozitiv')
+else :
+    print('numărul este negativ')
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este par și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number %2==0:
+    print ('number este par')
+else : 
+    print ('number nu este par')
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă numărul este impar și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if number %2!=0:
+    print('number nu este par')
+else :
+    print ('number este par')
 # CODUL TĂU VINE MAI SUS:
 
 # Creați o variabilă text și atribuiți-i un șir de caractere.
 
 # CODUL TĂU VINE MAI JOS:
-
+text = 'I not like homework!'
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
-
+print(text)
 # CODUL TĂU VINE MAI JOS:
-
+if "python" in text :
+    print ('textul conține cuvântul "Python"')
+else :
+    print('textul nu conține cuvântul "Python"')
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Java" și afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "java" in text :
+    print ('textul conține cuvântul "Java"')
+else :
+    print('textul nu conține cuvântul "Java"')
 # CODUL TĂU VINE MAI SUS:
+
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și afișați un mesaj corespunzător.
 # în cazul în care nu conține, verificați dacă conține cuvântul "Java" și afișați un mesaj corespunzător.
 # Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
-
+if "python" in text:
+    print('Textul conține cuvântul "Python"')
+elif "java" in text:
+    print('Textul conține cuvântul "Java"')
+else:
+    print('Textul nu conține cuvântul "Python" sau "Java"')
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind o instrucțiune condițională, verificați dacă textul conține cuvântul "Python" și cuvântul "Java" și afișați un mesaj corespunzător.
@@ -55,6 +74,7 @@
 # Dacă nu conține niciunul dintre ele, afișați un mesaj corespunzător.
 
 # CODUL TĂU VINE MAI JOS:
+
 
 # CODUL TĂU VINE MAI SUS:
 
