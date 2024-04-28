f7313fc82944f3d7001a16816f69a66ae1099f34
valyagag <valentina.gagauz17@gmail.com>
2024-04-29 00:17:10 +0300

    1/2 done

--- lessons/lesson8/task_1 (5).py
+++ lessons/lesson8/task_1 (5).py
@@ -3,58 +3,86 @@
 # Creați o listă de numere de la 1 la 10 folosind un for loop și funcția range().
 
 # CODUL TĂU VINE MAI JOS:
-
+lista= [1,2,3,4,5,6,7,8,9,10]
 # CODUL TĂU VINE MAI SUS:
-
+for i in range(len(lista)):
+    print(lista[i])
 # Folosind un for loop, parcurgeți lista creată și afișați numai numerele pare.
 
 # CODUL TĂU VINE MAI JOS:
-
+for i in lista:
+    if i % 2==0:
+        print(i)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind un while loop, creați o variabilă 'i' inițializată cu 1 și incrementați-o până când ajunge la 5. Afișați valoarea 'i' la fiecare pas.
 
 # CODUL TĂU VINE MAI JOS:
-
+i=1
+while i<6:
+    print (i)
+    i+=1
 # CODUL TĂU VINE MAI SUS:
 
 # Creați un dicționar person cu cheile 'name', 'age' și 'city' și iterați prin dicționar afișând perechile de cheie-valoare.
 
 # CODUL TĂU VINE MAI JOS:
-
+person= {'name':'Valentina',
+         'age':29,
+          'city': 'Here' }
+for key, value in person.items():
+    print(key,value)
 # CODUL TĂU VINE MAI SUS:
 
 # Utilizați un for loop pentru a itera printr-o listă de liste (matrice) și afișați fiecare element.
 
 # CODUL TĂU VINE MAI JOS:
-
+matrice= [[1,2,3],
+          [4,5,6], 
+          [7,8,9]]
+for row in matrice :
+    print(f"row: {row}")
+    for num in row :
+        print(f"number: {num}")
 # CODUL TĂU VINE MAI SUS:
 
 # Creați o secvență de numere folosind funcția range() și apoi iterați prin această secvență folosind un for loop pentru a afișa numerele.
 
 # CODUL TĂU VINE MAI JOS:
-
+numere=[i for i in range(20) ]
+for i in numere:
+    print(i)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosiți funcția enumerate() pentru a itera printr-o listă și a afișa indexul fiecărui element alături de valoarea sa.
 
 # CODUL TĂU VINE MAI JOS:
-
+for i, numere in enumerate(numere):
+    print(i,numere)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosiți funcția zip() pentru a itera simultan prin două liste și a afișa elementele corespunzătoare.
 
 # CODUL TĂU VINE MAI JOS:
-
+order=['juce', 'pasta', 'salad']
+count=[1,2,3,4,5,6]
+for name, numere in zip (order, count):
+    print(name, numere)
 # CODUL TĂU VINE MAI SUS:
 
 # Creați o listă de numere de la 1 la 10 folosind un for loop și funcția range().
 
 # CODUL TĂU VINE MAI JOS:
-
+lista= [num for num in range(1,11)]
+print(lista)
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind un buclă while, dublează valorile listei până când primul element va deveni mai mare decât 50.
+
+while lista[0] <= 50:
+    for i, numar in enumerate(lista):
+        lista[i] = numar * 2
+    print('numar dublat', lista)
 
 # CODUL TĂU VINE MAI JOS:
 
@@ -63,12 +91,19 @@
 # Generează și printează o listă cu toate numerele pătrat perfect din intervalul [1, 100].
 
 # CODUL TĂU VINE MAI JOS:
+numere_patrat = [num for num in range(1, 101) if int(num ** 0.5) == num ** 0.5]
+
+print("numbers:" ,numere_patrat)
 
 # CODUL TĂU VINE MAI SUS:
 
 # Folosind un buclă for , printează tabla înmulțirii pentru numărul 7.
 
 # CODUL TĂU VINE MAI JOS:
+for num in range(1, 11):
+    x = num * 7
+    print(num, "* 7 =",  x)
+
 
 # CODUL TĂU VINE MAI SUS:
 
